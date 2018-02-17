6e6140c731de04d3c28cd74406869b9d5fc2d103
MaximChernenko <twiggyramirez111@gmail.com>
2018-02-17 15:04:32 +0200

    End impl of Test method

--- TrpzSortedContainer/Program.cs
+++ TrpzSortedContainer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace TrpzSortedContainer
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[] { 3, 5, 6, 1, 10, 8, 0 };
+            TestSort(arr, SortetContainer.BubbleSort);
+            TestSort(arr, SortetContainer.InsertionSort);
+            TestSort(arr, SortetContainer.MergeSort);
+            TestSort(arr, SortetContainer.ShakerSort);
+            Console.ReadKey();
+        }
+
+        private static void TestSort(int[] arr, SortMethod method)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            method(arr);
+            watch.Stop();
+            TimeSpan span = watch.Elapsed;
+            Console.WriteLine("Затраченое время на сортировку {0} : {1}", method.Method.Name, span.Ticks);
         }
     }
 }
