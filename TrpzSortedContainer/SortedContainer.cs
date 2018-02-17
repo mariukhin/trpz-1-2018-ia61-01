6b38fe584dc06fda8973c5c7b6da197e18cd9819
maxim_mariukhin <maryuhin.z@gmail.com>
2018-02-17 17:21:59 +0200

    create function BubbleSort

--- TrpzSortedContainer/SortedContainer.cs
+++ TrpzSortedContainer/SortedContainer.cs
@@ -8,39 +8,10 @@ namespace TrpzSortedContainer
 {
     public class SortedContainer
     {
-        //Сортировка перемешиванием
-        public static int[] ShakerSort(int[] arr)
-        {
-            int left = 0,
-            right = arr.Length - 1,
-            count = 0;
-
-            while (left <= right)
-            {
-                for (int i = left; i < right; i++)
-                {
-                    count++;
-                    if (arr[i] > arr[i + 1])
-                        Swap(arr, i, i + 1);
-                }
-                right--;
-
-                for (int i = right; i > left; i--)
-                {
-                    count++;
-                    if (arr[i - 1] > arr[i])
-                        Swap(arr, i - 1, i);
-                }
-                left++;
-            }
-            return arr;
-        }
-
-        private static void Swap(int[] myint, int i, int j)
-        {
-            int glass = myint[i];
-            myint[i] = myint[j];
-            myint[j] = glass;
-        }
-    }
+		//Cортировка пузырьком
+		public static int[] BubbleSort(int[] arr)
+		{
+			
+		}
+	}
 }
