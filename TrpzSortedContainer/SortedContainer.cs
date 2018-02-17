8d05e7c0cee47bdd47f1b8542084299619547bb0
MaximChernenko <twiggyramirez111@gmail.com>
2018-02-17 14:57:03 +0200

    Init of method ShakerSort

---
+++ TrpzSortedContainer/SortedContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrpzSortedContainer
+{
+    public class SortedContainer
+    {
+        //Сортировка перемешиванием
+        public static int[] ShakerSort(int[] arr)
+        {
+        }
+    }
+}
