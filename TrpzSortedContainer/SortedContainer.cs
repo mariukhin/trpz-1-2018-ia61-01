756d4f20f76e3fe57097bcf1552f399db0c93d8c
maxim_mariukhin <maryuhin.z@gmail.com>
2018-02-17 17:23:33 +0200

    final commit

--- TrpzSortedContainer/SortedContainer.cs
+++ TrpzSortedContainer/SortedContainer.cs
@@ -11,7 +11,20 @@ namespace TrpzSortedContainer
 		//Cортировка пузырьком
 		public static int[] BubbleSort(int[] arr)
 		{
-			
+			int temp;
+			for (int i = 0; i < arr.Length - 1; i++)
+			{
+				for (int j = i + 1; j < arr.Length; j++)
+				{
+					if (arr[i] > arr[j])
+					{
+						temp = arr[i];
+						arr[i] = arr[j];
+						arr[j] = temp;
+					}
+				}
+			}
+			return arr;
 		}
 	}
 }
