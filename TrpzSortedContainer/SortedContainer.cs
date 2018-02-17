f6f07f8ec41af8d981207f6b36ff0584b1a4aea3
Kostya <boloya@ukr.net>
2018-02-17 19:07:41 +0200

    First commit.

--- TrpzSortedContainer/SortedContainer.cs
+++ TrpzSortedContainer/SortedContainer.cs
@@ -8,57 +8,10 @@ namespace TrpzSortedContainer
 {
 	public class SortedContainer
 	{
-		//Сортировка перемешиванием
-		public static int[] ShakerSort(int[] arr)
-		{
-			int left = 0,
-			right = arr.Length - 1,
-			count = 0;
-
-			while (left <= right)
-			{
-				for (int i = left; i < right; i++)
-				{
-					count++;
-					if (arr[i] > arr[i + 1])
-						Swap(arr, i, i + 1);
-				}
-				right--;
-
-				for (int i = right; i > left; i--)
-				{
-					count++;
-					if (arr[i - 1] > arr[i])
-						Swap(arr, i - 1, i);
-				}
-				left++;
-			}
-			return arr;
-		}
-		private static void Swap(int[] myint, int i, int j)
-		{
-			int glass = myint[i];
-			myint[i] = myint[j];
-			myint[j] = glass;
-		}
-
-		//Cортировка пузырьком
-		public static int[] BubbleSort(int[] arr)
-		{
-			int temp;
-			for (int i = 0; i < arr.Length - 1; i++)
-			{
-				for (int j = i + 1; j < arr.Length; j++)
-				{
-					if (arr[i] > arr[j])
-					{
-						temp = arr[i];
-						arr[i] = arr[j];
-						arr[j] = temp;
-					}
-				}
-			}
-			return arr;
-		}
-	}
+        //Сортировка вставками
+        public static int[] InsertionSort(int[] arr)
+        {
+           
+        }
+    }
 }
